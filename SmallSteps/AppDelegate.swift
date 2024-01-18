2442927d7d1469cf50216c52c07e5370d2fca60d
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-01-18 21:30:29 +0530

    User Defaults(start)

--- SmallSteps/AppDelegate.swift
+++ SmallSteps/AppDelegate.swift
@@ -14,7 +14,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
         return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("EnteredBackground")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("Terminated")
     }
 
     // MARK: UISceneSession Lifecycle
--- SmallSteps/ToDoViewController.swift
+++ SmallSteps/ToDoViewController.swift
@@ -11,9 +11,13 @@ class ToDoViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK: - Tableview datasource methods
@@ -54,6 +58,8 @@ class ToDoViewController: UITableViewController {
         let action = UIAlertAction(title: "Add item", style: .default) { action in
             //what will happen once the user clicks the add item button on ui alert
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
